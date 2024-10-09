84c67efae0a58883bc21765859f968ae2c29fc2f
Michael Ferguson <mfergs7@gmail.com>
2024-10-09 10:57:09 -0400

    load MTC task capability

--- ubr1_moveit/launch/move_group.launch.py
+++ ubr1_moveit/launch/move_group.launch.py
@@ -4,4 +4,5 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("ubr1", package_name="ubr1_moveit").to_moveit_configs()
+    moveit_config.move_group_capabilities["capabilities"] = ["move_group/ExecuteTaskSolutionCapability"]
     return generate_move_group_launch(moveit_config)
