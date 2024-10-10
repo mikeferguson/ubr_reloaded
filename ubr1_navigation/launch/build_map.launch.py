6f170909d1ae65006c9ae8f9348b78b03ace791c
Michael Ferguson <mfergs7@gmail.com>
2024-10-10 10:17:23 -0400

    mapping is now lifecycle node

--- ubr1_navigation/launch/build_map.launch.py
+++ ubr1_navigation/launch/build_map.launch.py
@@ -1,7 +1,12 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
+                            RegisterEventHandler)
+from launch.events import matches_action
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -13,19 +18,48 @@ def generate_launch_description():
         default_value='false',
         description='Use simulation/Gazebo clock')
 
-    start_sync_slam_toolbox_node = Node(
+    start_sync_slam_toolbox_node = LifecycleNode(
         parameters=[
           get_package_share_directory("ubr1_navigation") + '/config/mapper_params_online_sync.yaml',
-          {'use_sim_time': use_sim_time}
+          {
+            'use_lifecycle_manager': False,
+            'use_sim_time': use_sim_time
+          }
         ],
         package='slam_toolbox',
         executable='sync_slam_toolbox_node',
         name='slam_toolbox',
-        output='screen')
+        namespace='',
+        output='screen'
+    )
+
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE
+        )
+    )
+
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=start_sync_slam_toolbox_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+                    transition_id=Transition.TRANSITION_ACTIVATE
+                ))
+            ]
+        )
+    )
 
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(start_sync_slam_toolbox_node)
+    ld.add_action(configure_event)
+    ld.add_action(activate_event)
 
     return ld
